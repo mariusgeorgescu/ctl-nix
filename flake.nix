20be1851b687a9ed9e63288d2a26f53e2f312d80
Walker <walker@praiseweb.com.br>
2022-11-07 09:39:27 -0300

    build: add flake and inputs

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Optioned Cardano Transaction Lib backed by nix setup";
+
+  inputs =
+    {
+      ctl.url = "github:Plutonomicon/cardano-transaction-lib";
+      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+      purs-nix.url = "github:purs-nix/purs-nix";
+      utils.url = "github:ursi/flake-utils";
+    };
+
+  outputs = { utils, ... }@inputs:
+    utils.apply-systems
+      { inherit inputs; }
+      ({ pkgs, ... }: { });
+}
