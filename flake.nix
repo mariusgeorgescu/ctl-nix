baa1c6f00c135348e85484fef5ad58ce899fdd0e
Walker <walker@praiseweb.com.br>
2022-11-28 10:33:51 -0300

    build: add pkgset and get-flake dep

--- flake.nix
+++ flake.nix
@@ -3,10 +3,13 @@
 
   inputs =
     {
-      ctl.url = "github:Plutonomicon/cardano-transaction-lib";
+      #ctl.url = "github:Plutonomicon/cardano-transaction-lib";
       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-      purs-nix.url = "github:purs-nix/purs-nix";
+      #purs-nix.url = "github:purs-nix/purs-nix";
       utils.url = "github:ursi/flake-utils";
+      get-flake.url = "github:ursi/get-flake";
+      package-set-repo.url = "github:purescript/package-sets";
+      package-set-repo.flake = false;
     };
 
   outputs = { utils, ... }@inputs:
