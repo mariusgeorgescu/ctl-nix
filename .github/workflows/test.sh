b07ab020a2144352603e38de48e62e7ea54cd441
Walker <walker@praiseweb.com.br>
2023-01-27 22:47:10 -0300

    test: rework test to use nix-templates

---
+++ .github/workflows/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+CTL=$(pwd)
+TEST_DIR=$(mktemp)
+set +ex
+rm -Rf $TEST_DIR
+set -ex
+mkdir $TEST_DIR
+(
+	cd $TEST_DIR
+	git init
+	nix flake init -t github:LovelaceAcademy/nix-templates#ctl
+	sed -i "s|github:LovelaceAcademy/ctl-nix|path:$CTL|g" flake.nix
+	nix flake check --show-trace
+	nix build --show-trace -L
+	node result/Main/index.js
+)
